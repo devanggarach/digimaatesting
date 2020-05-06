77e0c478622158266af59db5074119f28e67cb6a
Devang Garach <devanggarach@gmail.com>
2020-05-06 08:27:03 +0530

    Add files via upload

---
+++ js/digimaa.js
@@ -0,0 +1,21 @@
+(function($) {
+    $(function() {
+      $('.toggle-overlay').click(function() {
+        $('aside').toggleClass('open');
+      });
+    });
+  })(jQuery);
+
+  $(window).bind('scroll', function() {
+    var currentTop = $(window).scrollTop();
+    var elems = $('.scrollspy');
+    elems.each(function(index){
+      var elemTop 	= $(this).offset().top;
+      var elemBottom 	= elemTop + $(this).height();
+      if(currentTop >= elemTop && currentTop <= elemBottom){
+        var id 		= $(this).attr('id');
+        var navElem = $('a[href="#' + id+ '"]');
+    navElem.parent().addClass('active').siblings().removeClass( 'active' );
+      }
+    })
+}); 
