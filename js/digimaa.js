c905be883fbf99c95b8304a1a7e0510eeceeed73
Devang Garach <devanggarach@gmail.com>
2020-05-08 05:18:44 +0530

    7

--- js/digimaa.js
+++ js/digimaa.js
@@ -8,30 +8,13 @@ last modified date - 5th May, 2020 */
       });
     });
   })(jQuery);
-
-/* index page section change anchor tag highlight js
-last code edited by - Devang
-last modified date - 5th May, 2020 */
-  $(window).bind('scroll', function() {
-    var currentTop = $(window).scrollTop();
-    var elems = $('.scrollspy');
-    elems.each(function(index){
-      var elemTop 	= $(this).offset().top;
-      var elemBottom 	= elemTop + $(this).height();
-      if(currentTop >= elemTop && currentTop <= elemBottom){
-        var id 		= $(this).attr('id');
-        var navElem = $('a[href="#' + id+ '"]');
-    navElem.parent().addClass('active').siblings().removeClass( 'active' );
-      }
-    })
-});
-
+  
 /* explore_services page section highlight js
 last code edited by - Devang
 last modified date - 6th May, 2020 */
 $(window).scroll(function(){
 
-  $('section').each(function() {
+  $('.back_section').each(function() {
       if($(window).scrollTop() >= $(this).offset().top + $(this).height() || $(window).scrollTop() < $(this).offset().top)
           $(this).addClass('highlight');
       else
@@ -39,3 +22,7 @@ $(window).scroll(function(){
   });
 
 });
+
+
+
+
