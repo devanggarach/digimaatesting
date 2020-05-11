b43b81c21345040a3e6c0f014b3c6a86f3d5aa52
Devang Garach <devanggarach@gmail.com>
2020-05-12 05:22:49 +0530

    11

--- js/digimaa.js
+++ js/digimaa.js
@@ -23,6 +23,9 @@ $(window).scroll(function(){
 
 });
 
-
-
-
+function checkInput(ob) {
+  var invalidChars = /[^0-9]/gi
+  if(invalidChars.test(ob.value)) {
+            ob.value = ob.value.replace(invalidChars,"");
+      }
+}
